584d3c9c5f064069ebf06afc8b9e2a3200fee7d5
JohnWSMartel <jwm3464@g.rit.edu>
2019-04-24 18:59:41 -0400

    Leaderboard setup, needs CSS changes and documentation

--- Drom-Leaderboard/Single Page/script.js
+++ Drom-Leaderboard/Single Page/script.js
@@ -11,35 +11,149 @@ let rank9name = document.querySelector("#teamNineName");
 let rank10name = document.querySelector("#teamTenName");*/
 
 var scores = {
-    rank1: {"name":"Team Name","score":"Team Score","streak":""},
-    rank2: {"name":"Team Name","score":"Team Score","streak":""},
-    rank3: {"name":"Team Name","score":"Team Score","streak":""},
-    rank4: {"name":"Team Name","score":"Team Score","streak":""},
-    rank5: {"name":"Team Name","score":"Team Score","streak":""},
-    rank6: {"name":"Team Name","score":"Team Score","streak":""},
-    rank7: {"name":"Team Name","score":"Team Score","streak":""},
-    rank8: {"name":"Team Name","score":"Team Score","streak":""},
-    rank9: {"name":"Team Name","score":"Team Score","streak":""},
-    rank10: {"name":"Team Name","score":"Team Score","streak":""},
-    temp: {"name":"","score":"","streak":""},
+    rank1: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank2: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank3: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank4: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank5: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank6: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank7: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank8: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank9: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    rank10: {
+        "name": "Team Name",
+        "score": "Team Score",
+        "streak": ""
+    },
+    temp: {
+        "name": "",
+        "score": "",
+        "streak": ""
+    },
 };
 
-window.onload = function(){
-  rank1name = "changed";
-  //Set the blank 
-  document.querySelector("#teamOneName").innerHTML = rank1name;
-}
+//window.onload = function () {
+//    rank1name = "changed";
+//    //Set the blank 
+//    document.querySelector("#teamOneName").innerHTML = rank1name;
+//}
 
 
 function rank() {
-  //Collect inputed data
-  let newTeam = document.querySelector("#team");
-  let newScore = document.querySelector("#score");
-  //Compare Scores
-  for(var i = 0; i < scores.length; i++){
-    
-  }
-  //When rank is found put name, score, and streak in appropriate ranking
-  
-  //Push last ranking out
+    //Collect inputed data
+    let newTeam = document.querySelector("#team");
+    let newScore = document.querySelector("#score");
+    //Compare Scores
+    for (var i = 0; i < scores.length; i++) {
+
+    }
+    //When rank is found put name, score, and streak in appropriate ranking
+
+    //Push last ranking out
+}
+
+const compare = function (a, b) {
+    if (a.score < b.score) {
+        return 1;
+    }
+    if (a.score > b.score) {
+        return -1;
+    }
+    return 0;
+}
+
+const getUpdate = () => {
+    $.ajax({
+        cache: false,
+        type: "GET",
+        url: '/getScores',
+        dataType: "json",
+        success: (result, status, xhr) => {
+            scores = result.scores;
+            scores.sort(compare);
+            
+            let loopTimes = 10;
+            if(scores.length < loopTimes) {
+                loopTimes = scores.length;
+            }
+            
+            let scoresDiv = document.querySelector("#scores");
+            scoresDiv.innerHTML = "";
+            for(let i = 0; i < loopTimes; i++) {
+                let div = document.createElement('div');
+                div.className = 'data';
+                
+                let number = document.createElement('p');
+                number.className = 'number';
+                number.innerHTML = `${i+1}`;
+                div.appendChild(number);
+                
+                let teamName = document.createElement('p');
+                teamName.className = 'teamName';
+                teamName.innerHTML = `${scores[i].team}`;
+                div.appendChild(teamName);
+                
+                let teamScore = document.createElement('p');
+                teamScore.className = 'teamScore';
+                teamScore.innerHTML = `${scores[i].score}`;
+                div.appendChild(teamScore);
+                
+                let teamStreak = document.createElement('p');
+                teamStreak.className = 'teamStreak';
+                teamStreak.innerHTML = `${scores[i].streak}`;
+                div.appendChild(teamStreak); 
+                
+                scoresDiv.appendChild(div);
+            }
+
+
+        },
+        error: (xhr, status, error) => {
+            const messageObj = JSON.parse(xhr.responseText);
+
+            handleError(messageObj.error);
+        }
+    });
+};
+
+//every 5 seconds call getUpdate
+window.onload = function () {
+    getUpdate();
+    setInterval(getUpdate, 5000);
 }
--- Drom-Leaderboard/server/controllers/scores.js
+++ Drom-Leaderboard/server/controllers/scores.js
@@ -3,9 +3,10 @@ const models = require('../models');
 const Scores = models.scores;
 
 const setScore = (req, res) => {
-    console.log(req.body);
   if (!req.body.team || !req.body.score || !req.body.streak) {
-    return res.status(400).json({ error: 'Not all fields are filled.' });
+    return res.status(400).json({
+      error: 'Not all fields are filled.'
+    });
   }
 
   const teamData = {
@@ -14,19 +15,25 @@ const setScore = (req, res) => {
     streak: req.body.streak,
   };
 
-  const newScore = new scores.scoreModel(teamData);
+  const newScore = new Scores.scoreModel(teamData);
 
   const scorePromise = newScore.save();
-    
-  scorePromise.then(() => res.status(200));
+
+  scorePromise.then(() => {
+    res.end();
+  });
 
   scorePromise.catch((err) => {
     console.log(err);
     if (err.code === 11000) {
-      return res.status(400).json({ error: 'Score already exists.' });
+      return res.status(400).json({
+        error: 'Score already exists.'
+      });
     }
 
-    return res.status(400).json({ error: 'An error occured' });
+    return res.status(400).json({
+      error: 'An error occured'
+    });
   });
 
   return scorePromise;
@@ -36,14 +43,17 @@ const getScores = (request, response) => {
   const req = request;
   const res = response;
 
-  //Don't think I need everything in the ()
-  return Scores.scoreModel.findAll(req.session.account._id, (err, docs) => {
+  return Scores.scoreModel.findAll((err, docs) => {
     if (err) {
       console.log(err);
-      return res.status(400).json({ error: 'An error occured' });
+      return res.status(400).json({
+        error: 'An error occured'
+      });
     }
 
-    return res.json({ domos: docs });
+    return res.json({
+      scores: docs
+    });
   });
 };
 
